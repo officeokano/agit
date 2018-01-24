d615c9d0f999d6935db2bc90af7a66ac8e4086b7
okano <h.okano@gmail.com>
2018-01-24 17:42:57 +0900

    backup files to home

--- backup.sh
+++ backup.sh
@@ -5,13 +5,13 @@ cd ..
 $current/agit.sh pull
 current=$(basename `pwd`)
 cd ..
-if [ -e gitbackup.tar.gz ]; then
-    if [ -e oldgitbackup.tar.gz ]; then
-        rm oldgitbackup.tar.gz
+if [ -e ~/gitbackup.tar.gz ]; then
+    if [ -e ~/gitbackup.old.tar.gz ]; then
+        rm ~/gitbackup.old.tar.gz
     fi
-    mv gitbackup.tar.gz oldgitbackup.tar.gz
+    mv ~/gitbackup.tar.gz ~/gitbackup.old.tar.gz
 fi
 printf "starting backup...\n"
-tar czf gitbackup.tar.gz --exclude='.*' $current
-gpg -e -r h.okano@gmail.com -o gitbackup.tar.gz.pgp gitbackup.tar.gz
+tar czf ~/gitbackup.tar.gz --exclude='.*' $current
+gpg -e -r h.okano@gmail.com -o ~/gitbackup.tar.gz.pgp ~/gitbackup.tar.gz
 printf "done\n"
