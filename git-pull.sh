303a4d19005689cb31d38fb3ae5536d148947fd6
okano <bbc.okano@gmail.com>
2020-03-31 10:13:50 +0900

    fix

--- git-pull.sh
+++ git-pull.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-files="*"
-dirary=()
 git=".git"
-for filepath in $files; do
-  if [ -d $filepath ] ; then
-    dirary+=("$filepath")
+for f in * ; do
+  if [ -d $f ] ; then
+    pushd $f > /dev/null
+    if [ -e $git ] ; then
+      printf "pull $f from remote\n"
+      git pull
+      printf "\n"
+    fi
+    popd > /dev/null
   fi
-done
-for i in ${dirary[@]}; do
-  pushd $i > /dev/null
-  if [ -e $git ]; then
-    printf "pull $i from remote\n"
-    git pull
-    printf "\n"
-  fi
-  popd > /dev/null
 done
